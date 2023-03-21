737d485c2fe049b34c16c7b11ce771e3547519e0
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-03-21 22:53:06 +0400

    Added launchscreen, color sets

---
+++ ImagesFeed/UIColor+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor+Extensions.swift
+//  ImagesFeed
+//
+//  Created by Вадим Кузьмин on 19.03.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var YPBackground: UIColor {
+        UIColor(named: "YPBackground") ?? UIColor.black
+    }
+    static var YPBlack: UIColor {
+        UIColor(named: "YPBlack") ?? UIColor.black
+    }
+    static var YPBlue: UIColor {
+        UIColor(named: "YPBlue") ?? UIColor.blue
+    }
+    static var YPGray: UIColor {
+        UIColor(named: "YPGray") ?? UIColor.gray
+    }
+    static var YPRed: UIColor {
+        UIColor(named: "YPRed") ?? UIColor.red
+    }
+    static var YPWhiteAlpha50: UIColor {
+        UIColor(named: "YPWhiteAlpha50") ?? UIColor.white
+    }
+}
