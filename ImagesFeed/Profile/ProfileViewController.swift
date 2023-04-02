25e73b27c2dfe3890fb1ced55d52f9764e5f6eb5
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-04-02 19:37:12 +0400

    Changed storyboard layout to manual layout in ProfileViewController

--- ImagesFeed/Profile/ProfileViewController.swift
+++ ImagesFeed/Profile/ProfileViewController.swift
@@ -9,13 +9,6 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
 
-    // MARK: - Outlets
-    @IBOutlet private weak var profileImage: UIImageView!
-    @IBOutlet private weak var profileName: UILabel!
-    @IBOutlet private weak var profileUserName: UILabel!
-    @IBOutlet private weak var profileDescription: UILabel!
-    @IBOutlet private weak var exitButton: UIButton!
-
     // MARK: - Public Properties
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -24,11 +17,82 @@ final class ProfileViewController: UIViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        exitButton.setTitle("", for: .normal)
-        exitButton.imageView?.contentMode = .scaleAspectFill
+
+        view.addSubview(imageView)
+        view.addSubview(nameLabel)
+        view.addSubview(loginLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(exitButton)
+
+        configureConstraints()
     }
 
-    // MARK: - Actions
-    @IBAction private func exitButtonTapped(_ sender: UIButton) {
+    // MARK: - Methods
+    @objc private func exitButtonTapped() {
+    }
+
+    // MARK: - Layout
+    private var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: Constants.UIImages.userPicture.rawValue)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = "Jane Doe"
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        nameLabel.textColor = .white
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        return nameLabel
+    }()
+
+    private var loginLabel: UILabel = {
+        let loginLabel = UILabel()
+        loginLabel.text = "@jane_doe"
+        loginLabel.font = UIFont.systemFont(ofSize: 13)
+        loginLabel.textColor = UIColor.YPGray
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        return loginLabel
+    }()
+
+    private var descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, World!"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.textColor = .white
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return descriptionLabel
+    }()
+
+    private var exitButton: UIButton = {
+        let image = UIImage(systemName: "ipad.and.arrow.forward") ?? UIImage()
+        let exitButton = UIButton.systemButton(with: image, target: nil, action: #selector(exitButtonTapped))
+        exitButton.imageView?.contentMode = .scaleAspectFill
+        exitButton.tintColor = UIColor.YPRed
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        return exitButton
+    }()
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalToConstant: 70),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+
+            exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -26),
+            exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+        ])
     }
 }
